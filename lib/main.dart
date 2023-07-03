18d58d0de75252d97e026ee59c6148900940b890
KRTalha <rana.talha20003@gmail.com>
2023-07-03 15:42:16 +0200

    day 3 is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:master_app/pages/login_page.dart';
+import 'pages/home_page.dart';
 void main (){
   runApp(MyApp());
 
@@ -9,17 +10,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    // bool isMale = true;
-    // num temp = 30.5;
-    //
-    // var day = "Tuesday";
-    // const pi= 3.14;
-
-
-
     return MaterialApp(
-      home: HomePage(),
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes : {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/Login" : (context) => LoginPage(),
+      }
     );
   }
 }
